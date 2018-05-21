6e7854a36aa1b56b82d2a28bf5358332d1cd37ed
Chiien <fuchienhsu12@gmail.com>
2018-05-21 13:37:04 -0300

    Add angular-material module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// MODULES
+import { AngularMaterialModule } from './angular-material/angular-material.module';
+
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +16,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AngularMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
