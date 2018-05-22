8d8298da75dd0be5772c81f62821cb0569f71911
Chiien <fuchienhsu12@gmail.com>
2018-05-21 22:17:32 -0300

    Add angular-material some components

--- src/app/angular-material/angular-material.module.ts
+++ src/app/angular-material/angular-material.module.ts
@@ -2,15 +2,30 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   imports: [
     CommonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatGridListModule,
+    MatButtonModule
   ],
   declarations: [],
   exports: [
-    MatToolbarModule
+    MatToolbarModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatGridListModule,
+    MatButtonModule
   ]
 })
 export class AngularMaterialModule { }
