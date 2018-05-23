58c31030260d458cd998ee4a5c9dd5d12b9ca579
Chiien <fuchienhsu12@gmail.com>
2018-05-23 00:19:30 -0300

    Add unit test to snackbar service

--- src/app/shared/snack-bar/snack-bar.service.spec.ts
+++ src/app/shared/snack-bar/snack-bar.service.spec.ts
@@ -1,15 +1,27 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from './../../angular-material/angular-material.module';
 import { TestBed, inject } from '@angular/core/testing';
 
 import { SnackBarService } from './snack-bar.service';
 
-fdescribe('SnackBarService', () => {
+describe('SnackBarService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [SnackBarService]
+      imports: [
+        AngularMaterialModule,
+        BrowserAnimationsModule
+      ],
+      providers: [
+        SnackBarService
+      ]
     });
   });
 
   it('should be created', inject([SnackBarService], (service: SnackBarService) => {
     expect(service).toBeTruthy();
+  }));
+
+  it('validate function openSnackBar', inject([SnackBarService], (service: SnackBarService) => {
+    expect(service.openSnackBar(`Failed`, `Close`)).toBeUndefined
   }));
 });
