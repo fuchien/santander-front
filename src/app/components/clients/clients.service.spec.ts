ae7b4ae40b92d2ccdb22dda99eeeb4de65ddac88
Chiien <fuchienhsu12@gmail.com>
2018-05-22 23:16:56 -0300

    Add unit test to clients services

--- src/app/components/clients/clients.service.spec.ts
+++ src/app/components/clients/clients.service.spec.ts
@@ -1,15 +1,23 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { ClientsService } from './clients.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('ClientsService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [ClientsService]
+      providers: [ClientsService],
+      imports: [
+        HttpClientTestingModule
+      ]
     });
   });
 
   it('should be created', inject([ClientsService], (service: ClientsService) => {
     expect(service).toBeTruthy();
+  }));
+
+  it('validate function getData', inject([ClientsService], (service: ClientsService) => {
+    expect(service.getData('')).not.toBeUndefined
   }));
 });
